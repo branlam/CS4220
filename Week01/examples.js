9168d3f07edd1a501633f6bf6a0699aa9f7d2b77
Auman <cydneymikel@gmail.com>
2019-01-28 06:31:01 -0800

    Week 01

---
+++ Week01/examples.js
@@ -0,0 +1,72 @@
+// console.log and Comments
+
+// console.log('hello'); // hello
+// console.log('hello' + ' ' + 'world') // hello world
+// console.log(1) // 1
+// console.log(1 + 1) // 2
+// console.log(false) // false
+// console.log(null) // null
+// console.log(undefined) // undefined
+
+
+// // Setting Empty
+// let x
+// console.log(x)
+// x = 1
+// console.log(x)
+// x = null
+// console.log(x)
+
+// // Comparison Operators
+// console.log('2' == 2)
+// console.log(false == 0)
+
+// console.log('2' === 2)
+// console.log(false === 0)
+
+// console.log(undefined === null)
+
+// console.log(2 > 1)
+// console.log(2 >= 2)
+// console.log(1 < 2)
+
+
+// // Unary Operators
+// console.log(typeof 2019)
+// console.log(typeof 'CS42200')
+
+// // Logical Operators
+// // &&
+// console.log(true && true) // true
+// console.log(true && false) // false
+
+// // ||
+// const value = 10
+// console.log(value || 5)
+
+// // !
+// console.log(!true)
+
+
+// // Declarations (let vs const)
+// const hello = 'hello'
+// // hello = 'hey' // <-- this will display an error when executed
+// console.log(hello)
+
+// let count = 1
+// count++
+// console.log(count)
+
+// let hey = 'hey'
+// hey = 'hello'
+// console.log(hey)
+
+
+// // Conditional and Indentation
+const number = 1
+if (true) {
+    if (number === 1)
+        console.log('print number is 1')
+    else
+        console.log('print number is not 1')
+}
