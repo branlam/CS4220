e955689c62fe93953ff0cccdcf891913ad9ee2d1
branlam <branlam@gmail.com>
2019-03-20 22:11:29 -0700

    started midterm

--- Week08/midterm_template/custom-cli-app/app.js
+++ Week08/midterm_template/custom-cli-app/app.js
@@ -1,2 +1,14 @@
 // application that allows uses the custom-module
 // allows the user to search, select a result, fetch details and display details
+const
+    superagent = require('superagent')
+
+async function search(name = ""){
+    const base = 'https://www.anapioficeandfire.com/api/characters'
+
+    //const charResponse = await superagent.get(`${base}/${name}`)
+    const charResponse = await superagent.get(`${base}`)
+    console.log(charResponse.body)
+}
+
+module.exports = { search }
--- Week08/midterm_template/custom-cli-app/cli.js
+++ Week08/midterm_template/custom-cli-app/cli.js
@@ -1,2 +1,22 @@
 // CLI using yargs for search command
 // ex: node cli.js search -k keyword
+const
+    yargs = require('yargs'),
+    app = require('./app')
+
+const flags = yargs.usage('$0: Usage <cmd> [options]')
+    .command({
+        command: 'search',
+        desc: 'search for characters in game of thrones',
+        builder: (yargs) => {
+            return yargs.option('n', {
+                alias: 'name',
+                describe: 'name of character'
+            })
+        },
+        handler: (argv) => { 
+            app.search(argv.name) 
+        }
+    })
+    .help('help')
+    .argv
